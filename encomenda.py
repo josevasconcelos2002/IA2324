2c9eabc01840064fb287c36bcbbc9126ad87d612
FabioCunha105089 <a105089@alunos.uminho.pt>
2023-11-18 16:16:57 +0000

    correcao

--- encomenda.py
+++ encomenda.py
@@ -1,7 +1,7 @@
 class Encomenda:
-    def __init__(self, idnt, cliente, peso, volume, deadline=0):
+    def __init__(self, idnt, client, weight, volume, deadline=0):
         self.idnt = idnt
-        self.cliente = cliente
-        self.peso = peso
+        self.cliente = client
+        self.peso = weight
         self.volume = volume
         self.deadline = deadline
