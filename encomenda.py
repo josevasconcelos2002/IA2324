b2203c9f8f5d7fc505e736bcc8a49611764ffc72
FabioCunha105089 <a105089@alunos.uminho.pt>
2023-11-18 16:15:48 +0000

    construtores

--- encomenda.py
+++ encomenda.py
@@ -1,2 +1,7 @@
 class Encomenda:
-    pass
+    def __init__(self, idnt, cliente, peso, volume, deadline=0):
+        self.idnt = idnt
+        self.cliente = cliente
+        self.peso = peso
+        self.volume = volume
+        self.deadline = deadline
--- estafeta.py
+++ estafeta.py
@@ -1,2 +1,5 @@
 class Estafeta:
-    pass
+    def __init__(self, idnt, vehicle):
+        self.idnt = idnt
+        self.vehicle = vehicle
+        self.rating = 5
