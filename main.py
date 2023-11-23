6f65c000ffaee1b5b45ea8c8b8ec4a9b90d35f20
Jose Vasconcelos <zevasconcelos6@gmail.com>
2023-11-23 18:28:03 +0000

    Interface criada com tkinter

--- main.py
+++ main.py
@@ -1,8 +1,88 @@
+import tkinter as tk
+from tkinter import ttk
 import networkx as nx
-import matplotlib.pyplot as plt
 from estafeta import Estafeta
 from encomenda import Encomenda
 import algoritmos as alg
+
+
+class Application:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Health Planet")
+        self.root.geometry("500x500")
+
+        # Adicionando a tela de boas-vindas com o logo
+        self.logo_label = tk.Label(
+            root, text="Health Planet", font=("Helvetica", 24))
+        self.logo_label.pack(pady=50)
+
+        # Adicionando um botão para continuar após o clique do usuário
+        self.continuar_btn = ttk.Button(
+            root, text="Clique para Continuar", command=self.mostrar_menu)
+        self.continuar_btn.pack(pady=20)
+
+        # Inicialmente, ocultar o menu de escolha do algoritmo
+        self.menu_frame = ttk.Frame(root)
+        self.menu_frame.pack_forget()
+
+        # Configurando o menu
+        self.label = ttk.Label(self.menu_frame, text="Escolha o algoritmo:")
+        self.label.pack(pady=10)
+
+        self.algoritmo_var = tk.StringVar()
+
+        self.radio_dfs = ttk.Radiobutton(
+            self.menu_frame, text="DFS", variable=self.algoritmo_var, value="dfs")
+        self.radio_dfs.pack()
+
+        self.radio_bfs = ttk.Radiobutton(
+            self.menu_frame, text="BFS", variable=self.algoritmo_var, value="bfs")
+        self.radio_bfs.pack()
+
+        self.radio_custo_uniforme = ttk.Radiobutton(
+            self.menu_frame, text="Custo Uniforme", variable=self.algoritmo_var, value="custo_uniforme")
+        self.radio_custo_uniforme.pack()
+
+        self.btn_executar = ttk.Button(
+            self.menu_frame, text="Executar", command=self.executar_algoritmo)
+        self.btn_executar.pack(pady=10)
+
+        self.btn_sair = ttk.Button(
+            self.menu_frame, text="Sair", command=root.destroy)
+        self.btn_sair.pack(pady=10)
+
+    def mostrar_menu(self):
+        # Esconder a tela de boas-vindas
+        self.logo_label.pack_forget()
+        self.continuar_btn.pack_forget()
+
+        # Mostrar o menu de escolha do algoritmo
+        self.menu_frame.pack(pady=50)
+
+    def executar_algoritmo(self):
+        escolha = self.algoritmo_var.get()
+
+        if escolha == "dfs":
+            algorithm = alg.dfs
+        elif escolha == "bfs":
+            algorithm = alg.bfs
+        elif escolha == "custo_uniforme":
+            algorithm = alg.custo_uniforme
+        else:
+            print("Escolha inválida.")
+            return
+
+        g, edges = build_graph()
+        est1 = Estafeta(1, 1)
+        enc1 = Encomenda(1, "Fabio", "3", "10", 3, 10)
+
+        visited, path, cost = algorithm(g, enc1.origin, enc1.destination)
+
+        print(f"Resultado do algoritmo escolhido:")
+        print(f"Visited: {visited}")
+        print(f"Path: {path}")
+        print(f"Cost: {cost} kms")
 
 
 def build_graph():
@@ -27,52 +107,7 @@ def build_graph():
     return g, edges
 
 
-def draw_graph(g, edges):
-    seed_value = 14
-    pos = nx.spring_layout(g, seed=seed_value)
-
-    plt.figure(figsize=(14, 11))
-    nx.draw(g, pos, with_labels=True, font_weight='bold')
-    edge_labels = {(edge[0], edge[1])
-                    : f"{edge[2]['distance']:.2f}" for edge in edges}
-    nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
-    plt.savefig("./dados/grafon.png", format="png")
-    plt.show()
-
-
-def main():
-    g, edges = build_graph()
-
-    print("\nEscolha o algoritmo:")
-    print("1. DFS")
-    print("2. BFS")
-    print("3. Custo Uniforme")
-    print("4. Sair")
-
-    escolha = input("\nDigite a opção desejada: ")
-
-    if escolha == "1":
-        algorithm = alg.dfs
-    elif escolha == "2":
-        algorithm = alg.bfs
-    elif escolha == "3":
-        algorithm = alg.custo_uniforme
-    elif escolha == "4":
-        return
-    else:
-        print("Escolha inválida. Saindo do programa.")
-        return
-
-    est1 = Estafeta(1, 1)
-    enc1 = Encomenda(1, "Fabio", "3", "10", 3, 10)
-
-    visited, path, cost = algorithm(g, enc1.origin, enc1.destination)
-
-    print(f"\nResultado do algoritmo escolhido:")
-    print(f"\nVisited: {visited}")
-    print(f"\nPath: {path}")
-    print(f"\nCost: {cost} kms")
-
-
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
