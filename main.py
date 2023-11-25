7a8e9c9e1ad2b885a71d77a3b8706f02592240e7
GabrielAraujo102023 <a102023@alunos.uminho.pt>
2023-11-25 17:57:06 +0000

    Adicionar estafeta

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from encomenda import Encomenda
 import algoritmos as alg
 import random
 
+ENCOMENDAS = []
+ESTAFETAS = []
+
 
 class Application:
     def __init__(self, root):
         self.root = root
         self.root.title("Health Planet")
         self.root.geometry("500x500")
+        self.current_frame = self.root
 
         # Adicionando a tela de boas-vindas com o logo
         self.logo_label = tk.Label(
@@ -23,43 +27,95 @@ class Application:
             root, text="Clique para Continuar", command=self.mostrar_menu)
         self.continuar_btn.pack(pady=20)
 
+        self.frame_menu_inicial = ttk.Frame(root)
+        self.frame_menu_inicial.pack_forget()
+
+        self.btn_criar_estafeta = ttk.Button(
+            self.frame_menu_inicial, text='Criar estafeta', command=self.mostrar_estafeta)
+        self.btn_criar_estafeta.pack(pady=10)
+
+        self.btn_algoritmos = ttk.Button(
+            self.frame_menu_inicial, text="Executar algoritmos", command=self.mostrar_algoritmos)
+        self.btn_algoritmos.pack(pady=20)
+
         # Inicialmente, ocultar o menu de escolha do algoritmo
-        self.menu_frame = ttk.Frame(root)
-        self.menu_frame.pack_forget()
+        self.frame_algoritmos = ttk.Frame(root)
+        self.frame_algoritmos.pack_forget()
 
         # Configurando o menu
-        self.label = ttk.Label(self.menu_frame, text="Escolha o algoritmo:")
+        self.label = ttk.Label(self.frame_algoritmos, text="Escolha o algoritmo:")
         self.label.pack(pady=10)
 
         self.algoritmo_var = tk.StringVar()
 
         self.radio_dfs = ttk.Radiobutton(
-            self.menu_frame, text="DFS", variable=self.algoritmo_var, value="dfs")
+            self.frame_algoritmos, text="DFS", variable=self.algoritmo_var, value="dfs")
         self.radio_dfs.pack()
 
         self.radio_bfs = ttk.Radiobutton(
-            self.menu_frame, text="BFS", variable=self.algoritmo_var, value="bfs")
+            self.frame_algoritmos, text="BFS", variable=self.algoritmo_var, value="bfs")
         self.radio_bfs.pack()
 
         self.radio_custo_uniforme = ttk.Radiobutton(
-            self.menu_frame, text="Custo Uniforme", variable=self.algoritmo_var, value="custo_uniforme")
+            self.frame_algoritmos, text="Custo Uniforme", variable=self.algoritmo_var, value="custo_uniforme")
         self.radio_custo_uniforme.pack()
 
         self.btn_executar = ttk.Button(
-            self.menu_frame, text="Executar", command=self.executar_algoritmo)
+            self.frame_algoritmos, text="Executar", command=self.executar_algoritmo)
         self.btn_executar.pack(pady=10)
 
-        self.btn_sair = ttk.Button(
-            self.menu_frame, text="Sair", command=root.destroy)
-        self.btn_sair.pack(pady=10)
+        self.btn_sair_algoritmos = ttk.Button(
+            self.frame_algoritmos, text="Sair", command=self.mostrar_menu)
+        self.btn_sair_algoritmos.pack(pady=10)
+
+        self.frame_estafeta = ttk.Frame(root)
+        self.frame_estafeta.pack_forget()
+
+        self.enc_label = ttk.Label(self.frame_estafeta, text="Estafeta:")
+        self.enc_label.pack(pady=10)
+
+        self.var_estafeta = tk.StringVar()
+
+        self.text_estafeta = tk.Text(self.frame_estafeta, height=1, width=20)
+        self.text_estafeta.pack(pady=10)
+
+        self.var_vehiculo = tk.IntVar()
+
+        self.radio_bicycle = ttk.Radiobutton(
+            self.frame_estafeta, text="Bicicleta", variable=self.var_vehiculo, value=1)
+        self.radio_bicycle.pack()
+
+        self.radio_bike = ttk.Radiobutton(
+            self.frame_estafeta, text="Mota", variable=self.var_vehiculo, value=2)
+        self.radio_bike.pack()
+
+        self.radio_car = ttk.Radiobutton(
+            self.frame_estafeta, text="Carro", variable=self.var_vehiculo, value=3)
+        self.radio_car.pack()
+
+        self.btn_criar_estafeta = ttk.Button(
+            self.frame_estafeta, text="Criar", command=self.save_estafeta)
+        self.btn_criar_estafeta.pack()
+
+        self.btn_sair_estafeta = ttk.Button(
+            self.frame_estafeta, text="Sair", command=self.mostrar_menu)
+        self.btn_sair_estafeta.pack(pady=10)
 
     def mostrar_menu(self):
         # Esconder a tela de boas-vindas
         self.logo_label.pack_forget()
         self.continuar_btn.pack_forget()
+        if self.current_frame != self.root:
+            self.current_frame.pack_forget()
 
+        self.current_frame = self.frame_menu_inicial
         # Mostrar o menu de escolha do algoritmo
-        self.menu_frame.pack(pady=50)
+        self.frame_menu_inicial.pack(pady=50)
+
+    def mostrar_algoritmos(self):
+        self.current_frame.pack_forget()
+        self.current_frame = self.frame_algoritmos
+        self.frame_algoritmos.pack(pady=50)
 
     def executar_algoritmo(self):
         escolha = self.algoritmo_var.get()
@@ -90,6 +146,23 @@ class Application:
         print(f"Visited: {visited}")
         print(f"Path: {path}")
         print(f"Cost: {cost} m")
+
+    def mostrar_estafeta(self):
+        self.current_frame.pack_forget()
+        self.current_frame = self.frame_estafeta
+        self.clean_estafeta_vars()
+        self.frame_estafeta.pack(pady=50)
+
+    def save_estafeta(self):
+        vehiculo = self.var_vehiculo.get()
+        nome = self.text_estafeta.get(1.0, "end-1c")
+        if vehiculo != 0 and nome != '':
+            ESTAFETAS.append(Estafeta(nome, vehiculo))
+            self.clean_estafeta_vars()
+
+    def clean_estafeta_vars(self):
+        self.text_estafeta.delete("1.0", "end")
+        self.var_vehiculo.set(0)
 
 
 if __name__ == "__main__":
