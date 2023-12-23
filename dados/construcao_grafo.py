b424142a783a866e26931b1b07c88dee26100ad7
Jose Vasconcelos <zevasconcelos6@gmail.com>
2023-12-23 09:51:13 +0000

    Construir grafo antes de testar

--- dados/construcao_grafo.py
+++ dados/construcao_grafo.py
@@ -1,60 +1,64 @@
 import networkx as nx
 import osmnx as ox
 
-# Constrói grafo usando o OpenStreetMap (OSM)
-g = ox.graph_from_place('Braga, Braga', network_type="drive")
 
-# Calcula distâncias entre nodos e adiciona-as às arestas
-ox.distance.add_edge_lengths(g)
+def construir_grafo():
+    # Constrói grafo usando o OpenStreetMap (OSM)
+    g = ox.graph_from_place('Braga, Braga', network_type="drive")
 
-# Remove ruas (arestas) sem nome do grafo
-edges = ox.graph_to_gdfs(g, nodes=False, edges=True)
-edges_filtered = edges[~edges['name'].apply(lambda x: isinstance(x, float))]
-edges_to_remove = set(g.edges) - set(edges_filtered.index)
-g.remove_edges_from(edges_to_remove)
+    # Calcula distâncias entre nodos e adiciona-as às arestas
+    ox.distance.add_edge_lengths(g)
 
-# Passo anterior resulta em vários nodos e arestas isolados
-# Aqui pegamos na maior componente ligada do grafo, removendo as componentes isoladas (nodos e arestas)
-g_un = g.to_undirected()
-largest_component = max(nx.connected_components(g_un), key=len)
-g_un = g_un.subgraph(largest_component)
+    # Remove ruas (arestas) sem nome do grafo
+    edges = ox.graph_to_gdfs(g, nodes=False, edges=True)
+    edges_filtered = edges[~edges['name'].apply(
+        lambda x: isinstance(x, float))]
+    edges_to_remove = set(g.edges) - set(edges_filtered.index)
+    g.remove_edges_from(edges_to_remove)
 
-# Mudar o nome (identificação) dos nodos para conveniência de utilização apenas
-# Inicialmente vêm com um número grande (osmid) como identificador. Aqui são numerados por ordem crescente
-nodes, edges = ox.graph_to_gdfs(g_un)
-node_rename_mapping = {old_node: str(new_node) for new_node, old_node in enumerate(g_un.nodes, start=0)}
-g_un = nx.relabel_nodes(g_un, node_rename_mapping)
+    # Passo anterior resulta em vários nodos e arestas isolados
+    # Aqui pegamos na maior componente ligada do grafo, removendo as componentes isoladas (nodos e arestas)
+    g_un = g.to_undirected()
+    largest_component = max(nx.connected_components(g_un), key=len)
+    g_un = g_un.subgraph(largest_component)
 
-# Remoção de atributos dados pelo OSM que não são importantes
-# Adicção de atributos extra
-g_un_g = nx.MultiGraph(g_un)
-for u, v, data in g_un_g.edges(data=True):
-    data['traffic'] = 1
-    data['blocked'] = False
-    data.pop('osmid', None)
-    data.pop('oneway', None)
-    data.pop('ref', None)
-    data.pop('geometry', None)
-    data.pop('maxspeed', None)
-    data.pop('bridge', None)
-    data.pop('lanes', None)
-    data.pop('highway', None)
-    data.pop('reversed', None)
-    data.pop('junction', None)
-    data.pop('access', None)
-    data.pop('tunnel', None)
-    data.pop('width', None)
-for node, data in g_un_g.nodes(data=True):
-    # data.pop('x', None)
-    # data.pop('y', None)
-    data.pop('ref', None)
-    data.pop('geometry', None)
-    data.pop('highway', None)
-    data.pop('street_count', None)
+    # Mudar o nome (identificação) dos nodos para conveniência de utilização apenas
+    # Inicialmente vêm com um número grande (osmid) como identificador. Aqui são numerados por ordem crescente
+    nodes, edges = ox.graph_to_gdfs(g_un)
+    node_rename_mapping = {old_node: str(
+        new_node) for new_node, old_node in enumerate(g_un.nodes, start=0)}
+    g_un = nx.relabel_nodes(g_un, node_rename_mapping)
 
-# Representação gráfico do grafo
-# fig, ax = ox.plot_graph(g_un, figsize=(25, 25))
-# fig.savefig("grafo.png", bbox_inches='tight')
+    # Remoção de atributos dados pelo OSM que não são importantes
+    # Adicção de atributos extra
+    g_un_g = nx.MultiGraph(g_un)
+    for u, v, data in g_un_g.edges(data=True):
+        data['traffic'] = 1
+        data['blocked'] = False
+        data.pop('osmid', None)
+        data.pop('oneway', None)
+        data.pop('ref', None)
+        data.pop('geometry', None)
+        data.pop('maxspeed', None)
+        data.pop('bridge', None)
+        data.pop('lanes', None)
+        data.pop('highway', None)
+        data.pop('reversed', None)
+        data.pop('junction', None)
+        data.pop('access', None)
+        data.pop('tunnel', None)
+        data.pop('width', None)
+    for node, data in g_un_g.nodes(data=True):
+        # data.pop('x', None)
+        # data.pop('y', None)
+        data.pop('ref', None)
+        data.pop('geometry', None)
+        data.pop('highway', None)
+        data.pop('street_count', None)
 
-# Guarda o grafo em formato gml num ficheiro
-nx.write_gml(g_un_g, 'grafo.gml')
+    # Representação gráfico do grafo
+    # fig, ax = ox.plot_graph(g_un, figsize=(25, 25))
+    # fig.savefig("grafo.png", bbox_inches='tight')
+
+    # Guarda o grafo em formato gml num ficheiro
+    nx.write_gml(g_un_g, './dados/grafo.gml')
--- testes.py
+++ testes.py
@@ -4,8 +4,11 @@ from encomenda import Encomenda
 from random import randint
 from estafeta import Estafeta
 from algoritmos import dijkstra, bfs
+from dados.construcao_grafo import construir_grafo
 import matplotlib.pyplot as plt
 import osmnx as ox
+
+construir_grafo()
 
 g = nx.read_gml('./dados/grafo.gml')
 
@@ -14,7 +17,8 @@ origin = str(nodes[0][0])
 maximum = len(nodes) - 1
 encomendas = []
 for i in range(30):
-    encomendas.append(Encomenda(i, None, origin, nodes[randint(0, maximum)], randint(1, 2), None, 350))
+    encomendas.append(Encomenda(
+        i, None, origin, nodes[randint(0, maximum)], randint(1, 2), None, 350))
 
 estafetas = []
 vehicle = 0
@@ -37,7 +41,8 @@ t = 0
 p = 0
 for k, (l, w) in s.items():
     if len(l) > 0:
-        print(f"Estafeta {k}, com vehiculo {estafetas[k].vehicle.value['type']}, tem as encomendas:")
+        print(
+            f"Estafeta {k}, com vehiculo {estafetas[k].vehicle.value['type']}, tem as encomendas:")
         for e in l:
             print(f"Encomenda {e.idnt}, peso {e.weight}")
             t += 1
@@ -52,7 +57,7 @@ print(r)
 print("Realizou route")
 
 # = ['b' if (u == 'ORIGIN') else 'yellow' for u, v, k in g.edges(keys=True)]
-#ox.plot_graph_routes(g, r[0], route_linewidth=6, node_size=0, route_alpha=1, node_color=node_color)
+# ox.plot_graph_routes(g, r[0], route_linewidth=6, node_size=0, route_alpha=1, node_color=node_color)
 for section, route in r.items():
     ox.plot_graph_routes(g, route, route_colors='yellow', route_linewidth=6, node_size=0, route_alpha=1,
                          show=False, save=True, filepath=f"./routes/section_{section}.png")
